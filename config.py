4af8903ee23b7c615f67b79d31fb1d2207d63b1c
Rsslnew <154116208+Rsslnew@users.noreply.github.com>
2024-04-17 11:50:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
+    APP_ID = int(os.environ.get("APP_ID", 20247370))
+    API_HASH = os.environ.get("API_HASH", "813309fab8cd9fce260ce7269e543bdb")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7153848219:AAF1nL2W3tTuFDKHjGU3N7rUE7NDOOJBO8Y")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1001881201311))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1748872441"))
+    MONGODB_URL = os.environ.get("MONGODB_URL", ")
     GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
