949c3d6e58a3794a645553bb0d08020204f02f18
Rsslnew <154116208+Rsslnew@users.noreply.github.com>
2024-04-17 11:57:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 20247370))
     API_HASH = os.environ.get("API_HASH", "813309fab8cd9fce260ce7269e543bdb")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7153848219:AAF1nL2W3tTuFDKHjGU3N7rUE7NDOOJBO8Y")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1001881201311))
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1001881201311"))
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 1748872441))
     MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://Set:12345@cluster0.ebhgmux.mongodb.net/?retryWrites=true&w=majority")
     GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN" "False")
