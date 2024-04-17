7e52805615ccf139773921e24133fda68f8ce567
Rsslnew <154116208+Rsslnew@users.noreply.github.com>
2024-04-17 11:57:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "813309fab8cd9fce260ce7269e543bdb")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7153848219:AAF1nL2W3tTuFDKHjGU3N7rUE7NDOOJBO8Y")
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1001881201311))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1748872441"))
-    MONGODB_URL = os.environ.get("MONGODB_URL", ")
-    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1748872441))
+    MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://Set:12345@cluster0.ebhgmux.mongodb.net/?retryWrites=true&w=majority")
+    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN" "False")
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
